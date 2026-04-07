86834bf943dfa2971230751983207bcb1eaa91f6
daywiss <david@adams.software>
2026-04-07 12:50:52 +0000

    chore: cleanup old files

--- msr/scripts/dev_env.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Compatibility wrapper. Prefer: source shared/scripts/dev_env.sh
-
-SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
-# shellcheck disable=SC1091
-source "$SCRIPT_DIR/../../shared/scripts/dev_env.sh" "$@"
